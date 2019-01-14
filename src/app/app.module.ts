5e179ef25f64825248110e6f0ad637b46ae597aa
Shchupakov Antony <a.shchupakov@mail.ru>
2019-01-14 12:32:45 +0500

    Добавляет навигацию

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,19 +12,30 @@ import { AskedPaymentsComponent } from './admin-panel/asked-payments/asked-payme
 import { CardPaymentItemComponent } from './admin-panel/card-payments/card-payment-item/card-payment-item.component';
 import { SafetyPipe } from './shared/safety.pipe';
 import { AskPaymentItemComponent } from './admin-panel/asked-payments/ask-payment-item/ask-payment-item.component';
-import {Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { InternetBankComponent } from './internet-bank/internet-bank.component';
-import {CompanyPromotionComponent} from './internet-bank/company/company-info/company-promotion/company-promotion.component';
-import {CompanyInfoComponent} from './internet-bank/company/company-info/company-info.component';
-import {CompanyHeaderComponent} from './internet-bank/company/company-header/company-header.component';
-import {PaymentComponent} from './internet-bank/payment/payment.component';
-import {PayBlockComponent} from './internet-bank/payment/pay-block/pay-block.component';
-import {AskForPaymentBlockComponent} from './internet-bank/payment/ask-for-payment-block/ask-for-payment-block.component';
-import {CardPaymentComponent} from './internet-bank/payment/pay-block/card-payment/card-payment.component';
-import {InternetBankPaymentComponent} from './internet-bank/payment/pay-block/internet-bank-payment/internet-bank-payment.component';
+import { CompanyPromotionComponent } from './internet-bank/company/company-info/company-promotion/company-promotion.component';
+import { CompanyInfoComponent } from './internet-bank/company/company-info/company-info.component';
+import { CompanyHeaderComponent } from './internet-bank/company/company-header/company-header.component';
+import { PaymentComponent } from './internet-bank/payment/payment.component';
+import { PayBlockComponent } from './internet-bank/payment/pay-block/pay-block.component';
+import { AskForPaymentBlockComponent } from './internet-bank/payment/ask-for-payment-block/ask-for-payment-block.component';
+import { CardPaymentComponent } from './internet-bank/payment/pay-block/card-payment/card-payment.component';
+import { InternetBankPaymentComponent } from './internet-bank/payment/pay-block/internet-bank-payment/internet-bank-payment.component';
 
 const appRoutes: Routes = [
-
+    { path: '', redirectTo: 'bank', pathMatch: 'full' },
+    { path: 'admin', component: AdminPanelComponent },
+    { path: 'bank', component: InternetBankComponent, children: [
+            { path: '', redirectTo: 'pay', pathMatch: 'full' },
+            { path: 'ask', component: AskForPaymentBlockComponent },
+            { path: 'pay', component: PayBlockComponent, children: [
+                    { path: '', redirectTo: 'card', pathMatch: 'full' },
+                    { path: 'card', component: CardPaymentComponent },
+                    { path: 'bank-payment', component: InternetBankPaymentComponent }
+                ]}
+        ]
+         }
 ];
 
 @NgModule({
@@ -49,7 +60,8 @@ const appRoutes: Routes = [
     imports: [
         BrowserModule,
         ReactiveFormsModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [CompanyService, HttpService],
     bootstrap: [AppComponent]
